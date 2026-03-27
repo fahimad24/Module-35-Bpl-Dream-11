fbbfd47d3cc936aa0c5f0033849b887b24bb236f
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-03-27 16:33:08 +0600

    navber create complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navber from "./components/Navber";
 
 function App() {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <>
+      <Navber />
+    </>
   );
 }
 
---
+++ src/components/Navber.jsx
@@ -0,0 +1,35 @@
+import logo from "../assets/logo.png";
+import dollar from "../assets/dollar.png";
+
+const Navber = () => {
+  return (
+    <nav className="pt-10 pb-5">
+      <div className="container mx-auto flex items-center justify-between">
+        <div>
+          <img src={logo} alt="Logo" />
+        </div>
+        <ul className="flex gap-8 justify-center items-center">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">Fixture</a>
+          </li>
+          <li>
+            <a href="/contact">Teams</a>
+          </li>
+          <li>
+            <a href="/contact">Schedules</a>
+          </li>
+          <li>
+            <button className="px-3 py-1.5 rounded-lg border flex items-center gap-2">
+              0 coin <img src={dollar} alt="Dollar" />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navber;
